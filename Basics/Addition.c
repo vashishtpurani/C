8b34f6cc17099c2214fd9b434aaa3647de1f5065
vashishtpurani <vashishtpurani@gmail.com>
2021-07-21 10:10:00 +0530

    New Commit

--- Basics/Addition.c
+++ Basics/Addition.c
@@ -1,6 +1,7 @@
-#include<stdio.h>
-void main(){
-	int a=10,b=20;
-	int sum = a+b;
-	printf("A + B =  %i\n",sum);
+#include <stdio.h>
+void main()
+{
+	int a = 10, b = 20;
+	int sum = a + b;
+	printf("A + B =  %i\n", sum);
 }
